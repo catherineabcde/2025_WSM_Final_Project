a83586c06bc9820cbce6a85ec72e2be30b06c231
eric31512 <eric31512@gmail.com>
2025-12-15 23:23:17 +0800

    test: test query rewrite(decompose)

--- My_RAG/main.py
+++ My_RAG/main.py
@@ -43,9 +43,9 @@ def main(query_path, docs_path, language, output_path):
 
     # Define rewrite mode
     if language == 'zh':
-        rewrite_mode = 'multi'
+        rewrite_mode = 'decompose'
     else:
-        rewrite_mode = 'multi' # or "multi", "hyde", "decompose", "none"
+        rewrite_mode = 'decompose' # or "multi", "hyde", "decompose", "none"
 
     for query in tqdm(queries, desc="Processing Queries"):
         # 4. Retrieve relevant chunks
